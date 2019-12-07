1cfcefdd79cdf0fd02442f17eda0ceddeda52713
kanukale, monika <kanukalemonika000@gmail.com>
2019-12-07 15:33:13 +0530

    web module

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,1 +1,29 @@
-// Include form validation functions here
+function validate() {
+	var val1 = document.forms["myform"]["txtName"].value;
+	if (val1 == "") {
+		alert("Title is required");
+		return false;
+	}
+	if(val1.length < 2 || (val1.length > 100) ) {
+		alert("Title should have 2 to 100 characters");
+		return false;
+	}
+	
+	var val2 = document.forms["myform"]["txtGross"].value;
+	if ( val2 == "" ) {
+		alert("Box Office is required");
+		return false;
+	}
+	var num = /[^0-9]/;
+	if( num.test(val2) ) {
+		alert("Box Office enter only numbers");
+		return false;
+	}
+	var val3 = document.forms["myform"]["dol"].value;
+	if (val3 == "") {
+		alert("Date of Launch is required");
+		return false;
+	}
+}
+
+	
